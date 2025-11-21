961e07c355d8e34e3b9364a7100d020858e2b2ce
Lucas Graeff Buhl-Nielsen <lucas.buhl-nielsen@quantco.com>
2025-11-21 11:24:42 +0000

    Don't cache TypeAdapter

--- src/openai/_models.py
+++ src/openai/_models.py
@@ -785,7 +785,8 @@ else:
 if not PYDANTIC_V1:
     from pydantic import TypeAdapter as _TypeAdapter
 
-    _CachedTypeAdapter = cast("TypeAdapter[object]", lru_cache(maxsize=None)(_TypeAdapter))
+    # _CachedTypeAdapter = cast("TypeAdapter[object]", lru_cache(maxsize=None)(_TypeAdapter))
+    _CachedTypeAdapter = _TypeAdapter
 
     if TYPE_CHECKING:
         from pydantic import TypeAdapter
